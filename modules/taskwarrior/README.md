08f5e790148104feea958a510382761b723a6ca6
tluxGhelew <tlux@ghelew.ch>
2015-11-12 22:30:37 +0100

    add taskwarrior modules

---
+++ modules/taskwarrior/README.md
@@ -0,0 +1,18 @@
+
+TASKWARRIOR
+============
+
+Provide useful alias with [GTD][1] in mind
+
+Aliases
+-------
+  - `t`         invoke `task` command
+  - `tin`       create a task with a tag "in" which represent an inbox item.
+  - `ttick`     create a task tickle that would show up in the inbox only at the defined time.
+  - `tmod`      modify a task
+  - `tthink`    create a task that would make you think about it a day later. The answer should always be yes or no!
+  _ `tsomeday`  list all task that are flag as someday. those tasks never appear in the `task next` query.
+
+
+[1]: https://en.wikipedia.org/wiki/Getting_Things_Done
+
---
+++ modules/taskwarrior/init.zsh
@@ -0,0 +1,35 @@
+###############################################################################
+# File:     init.zsh
+# Version   0.1
+# Author:   Thierry Ghelew <tlux@ghelew.net>
+# Licence:  GPLv2
+###############################################################################
+if [[ ! $+functions[tickle]  || ! $+functions[task-action] ]]; then
+    return 1
+fi
+
+alias t='task'
+alias tin='t +in add'
+alias tthink='tickle +1d'
+alias tsomeday='t +someday wait:someday add'
+
+function tmodify() {
+    local id=$1
+    shift
+    task-action $id modify $@
+}
+alias tmod='tmodify'
+
+function tdone() {
+    local id=$1
+    shift
+    task-action $id done $@
+}
+
+function tdelete() {
+    local id=$1
+    shift
+    task-action $id delete $@
+}
+alias tdel='tdelete'
+
