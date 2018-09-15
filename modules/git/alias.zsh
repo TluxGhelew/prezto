327b47da64175fb1064d1a1428589dba0291ff55
Thierry Ghelew <tlux@ghelew.net>
2018-09-15 16:19:41 -0400

    use a better format for alias glb (brief)

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -16,7 +16,7 @@ zstyle -s ':prezto:module:git:log:medium' format '_git_log_medium_format' \
 zstyle -s ':prezto:module:git:log:oneline' format '_git_log_oneline_format' \
   || _git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
 zstyle -s ':prezto:module:git:log:brief' format '_git_log_brief_format' \
-  || _git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
+  || _git_log_brief_format='%Cred%h%Creset-%C(yellow)%d%Creset%s%Cgreen(%cr)%C(bold blue)<%an>%Creset'
 
 # Status
 zstyle -s ':prezto:module:git:status:ignore' submodules '_git_status_ignore_submodules' \
@@ -101,12 +101,12 @@ alias giX='git rm -rf --cached'
 
 # Log (l)
 alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
-alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
-alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
-alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_format}"'
-alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
-alias glb='git log --topo-order --pretty=format:"${_git_log_brief_format}"'
+alias glb='git log --abbrev-commit --graph --date=relative --pretty=format:"${_git_log_brief_format}"'
 alias glc='git shortlog --summary --numbered'
+alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
+alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
+alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_format}"'
+alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
 
 # Merge (m)
 alias gm='git merge'
